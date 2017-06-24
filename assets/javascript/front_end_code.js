5ca1ba3f413625076b24959ad00589fbc268dd66
Chandra Kellwan <Chandra@Chandras-MacBook-Pro.local>
2017-06-24 18:00:00 -0400

    fixed zip code and pref css

--- assets/javascript/front_end_code.js
+++ assets/javascript/front_end_code.js
@@ -1,3 +1,14 @@
+var zipCode = "";
+var isValidZip = "";
+
 $('#btnGoZipCode').on('click', function(){
-	
+	zipCode = $('#name-input').val();
+	isValidZip = /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(zipCode);
+
+	if(isValidZip){
+		
+	}else{
+		alert(zipCode + " is not a valid zip code");
+	}
 });
+
