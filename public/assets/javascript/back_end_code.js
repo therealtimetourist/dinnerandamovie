4fe847cacdd963182e0d38a09a90a74b702605d6
Marcy <mlsantoyo@att.net>
2017-06-28 11:24:03 -0400

    changed movie API to latlong plus 10mi radius

--- public/assets/javascript/back_end_code.js
+++ public/assets/javascript/back_end_code.js
@@ -296,9 +296,8 @@ function pullMovies() {
   var date = moment().format('YYYY-MM-DD');
 
 //need to pass input zip code to this var
-  var zipCode = '32825';
-  var queryURL = 'http://data.tmsapi.com/v1.1/movies/showings?startDate=' +
-      date + '&zip=' + zipCode + '&api_key=52hkegdyrb7rrj8eraadpwg4';
+  var zipCode = '';
+  var queryURL = 'http://data.tmsapi.com/v1.1/movies/showings?startDate=' + date + '&lat=' + currentLat + '&lng=' + currentLong + '&radius=10&api_key=52hkegdyrb7rrj8eraadpwg4';
 
   $.ajax({
     url: queryURL,
